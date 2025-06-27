0852f86544aa9c3757e570a8cdd7e5872404567b
sindhuja <upputurisindhuja@gmail.com>
2025-06-27 15:31:35 +0530

    hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "trump: hey sindhu, how are you?"
+echo "sindhu: i'm fine, thank you! how are you?"
+echo "trump: i'm doing great, thanks for asking!"
+echo "sindhu: so, what are you up to these days?"
+echo "trump: just working on some projects and enjoying life. how about you?"
