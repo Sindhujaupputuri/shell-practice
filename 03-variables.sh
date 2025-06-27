7507bf4ec0b756d39f3a2fa50087398e611ca7a2
sindhuja <upputurisindhuja@gmail.com>
2025-06-27 15:41:51 +0530

    hello world started

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+PERSON1="trump"
+PERSON2="sindhu"
+
+echo "$PERSON1: hey $PERSON2, how are you?"
+echo "$PERSON2: i'm fine, thank you! how are you?"
+echo "$PERSON1: i'm doing great, thanks for asking!"
+echo "$PERSON2: so, what are you up to these days?"
+echo "$PERSON1: just working on some projects and enjoying life. how about you?"
