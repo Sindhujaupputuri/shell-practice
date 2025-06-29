7e7feb7386b7ede0e344dcbfed6d6685b7d181c3
sindhuja <upputurisindhuja@gmail.com>
2025-06-29 09:43:34 +0530

    special-vars

--- 07-special-vars.sh
+++ 07-special-vars.sh
@@ -2,3 +2,11 @@
 
 echo "All variables passed to the script: $*"
 echo "Number of variables passed to the script: $#"
+echo "script name: $0"
+echo "currect working directory: $PWD"
+echo "current user: $USER"
+echo "home directory: $HOME"
+echo "PID of the current process: $$"
+echo "PID of the last process: $!"
+sleep 10 &
+echo "PID of the last process: $!"
